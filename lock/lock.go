fc05d4d9a654dbd63e86c0243e50f1476b886d7a
fukuchidaisuke <fukuchidaisuke@realglobe.jp>
2014-02-18 15:40:07 +0900

    ロックファイル式ロックのパッケージを追加

---
+++ lock/lock.go
@@ -0,0 +1,54 @@
+package lock
+
+import (
+	"github.com/realglobe-Inc/go-lib-rg/erro"
+	"github.com/realglobe-Inc/go-lib-rg/log"
+	"os"
+	"syscall"
+)
+
+// ロックファイル式のロック。
+
+type Locker os.File
+
+// ロックするまで待つ。
+func Lock(path string) (*Locker, error) {
+	return lock(path, 0)
+}
+
+// ロックできなかったら nil を返す。
+func TryLock(path string) (*Locker, error) {
+	return lock(path, syscall.LOCK_NB)
+}
+
+func lock(path string, flag int) (*Locker, error) {
+	file, err := os.Create(path)
+	if err != nil {
+		return nil, erro.Wrap(err)
+	}
+
+	if e := syscall.Flock(int(file.Fd()), syscall.LOCK_EX|flag); e != nil {
+		file.Close()
+
+		if e == syscall.EWOULDBLOCK {
+			return nil, nil
+		}
+
+		return nil, erro.Wrap(err)
+	}
+
+	log.Debug("Locked ", file.Name(), ".")
+	return (*Locker)(file), nil
+}
+
+func (lock *Locker) Unlock() error {
+	file := (*os.File)(lock)
+	err := syscall.Flock(int(file.Fd()), syscall.LOCK_UN)
+	if err != nil {
+		return erro.Wrap(err)
+	}
+	file.Close()
+
+	log.Debug("Unlocked ", file.Name(), ".")
+	return nil
+}
---
+++ lock/lock_test.go
@@ -0,0 +1,59 @@
+package lock
+
+import (
+	"github.com/realglobe-Inc/daiku/common"
+	"io/ioutil"
+	"os"
+	"testing"
+)
+
+func TestLock(t *testing.T) {
+	file, err := ioutil.TempFile("", common.TestLabel)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer os.Remove(file.Name())
+	if e := file.Close(); e != nil {
+		t.Fatal(e)
+	}
+
+	n := 10
+	loop := 1000
+
+	counter := 0
+
+	ch := make(chan int)
+	lockPath := file.Name()
+	for i := 0; i < n; i++ {
+		go func(id int) {
+			defer func() { ch <- 0 }()
+
+			for j := 0; j < loop; j++ {
+				var lock *Locker
+				for lock == nil {
+					var err error
+					lock, err = Lock(lockPath)
+					if err != nil {
+						t.Error(id, j, err)
+						return
+					}
+				}
+
+				counter++
+				if e := lock.Unlock(); e != nil {
+					t.Error(id, j, e)
+					return
+				}
+			}
+		}(i)
+	}
+
+	// 終了待ち。
+	for i := 0; i < n; i++ {
+		<-ch
+	}
+
+	if counter != n*loop {
+		t.Error(counter, n, loop, file.Name())
+	}
+}
