f046bbe1f1e06b39d10dcbfb8a88a44e5b9532e1
fukuchidaisuke <fukuchidaisuke@realglobe.jp>
2014-04-04 19:20:52 +0900

    syslog の挙動を調べるためのコードを追加

--- rglog/handler/syslog_test.go
+++ rglog/handler/syslog_test.go
@@ -17,3 +17,25 @@ func TestSyslogHundler(t *testing.T) {
 	hndl.Output(0, level.ERR, "test2")
 	hndl.Flush()
 }
+
+// TODO 複数のコネクションで大量にログを吐くとデッドロックする場合がある。対処法不明。
+func _TestSyslogHandler(t *testing.T) {
+	n := 20
+	loop := 100
+
+	hndls := []Handler{}
+	for i := 0; i < n; i++ {
+		hndl, err := NewSyslogHandler("a")
+		if err != nil {
+			t.Fatal(err)
+		}
+		hndl.SetLevel(level.ALL)
+		hndls = append(hndls, hndl)
+	}
+
+	for i := 0; i < loop; i++ {
+		for j := 0; j < len(hndls); j++ {
+			hndls[j].Output(0, level.ERR, "a ", j, i)
+		}
+	}
+}
