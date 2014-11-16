d88cda1ce77bb478101edffe671fda8effcdaef2
fukuchidaisuke <fukuchidaisuke@realglobe.jp>
2014-11-16 22:11:07 +0900

    ファイル書き込みが失敗してもすぐにはログを捨てないようにした

--- rglog/handler/rotate.go
+++ rglog/handler/rotate.go
@@ -1,7 +1,8 @@
 package handler
 
 import (
-	"bufio"
+	"container/list"
+	"errors"
 	"fmt"
 	"github.com/realglobe-Inc/go-lib-rg/erro"
 	"github.com/realglobe-Inc/go-lib-rg/rglog/level"
@@ -21,8 +22,6 @@ const (
 type rotateCoreHandler struct {
 	// ログファイルのパス。
 	path string
-	// ログファイル 1 つの最大サイズ。
-	limit int64
 	// ログファイルを最大でいくつバックアップするか。
 	num int
 	// ログの書式。
@@ -32,90 +31,76 @@ type rotateCoreHandler struct {
 
 	// 今開いているファイル。
 	file *os.File
-	// 今のサイズ。
-	size int64
-	// 書き込み口。
-	sink *bufio.Writer
+	// バッファ兼書き込み口。
+	buff *fileLogBuffer
 }
 
 func (core *rotateCoreHandler) output(file string, line int, lv level.Level, v ...interface{}) {
-	if err := core.outputCore(file, line, lv, v...); err != nil {
-		err = erro.Wrap(err)
-		fmt.Fprintln(os.Stderr, err)
-		if core.sink != nil {
-			core.sink.Flush()
-			core.sink = nil
-		}
-		if core.file != nil {
-			core.file.Close()
-			core.file = nil
-		}
-	}
+	buff := core.fmter.Format(time.Now(), file, line, lv, v...)
+
+	const ( // てきとう。
+		writeSize  = 4096
+		bufferSize = 2*writeSize + 1024
+	)
+
+	core.buff.add(buff)
+	defer core.buff.trim(bufferSize)
+	core.flushCore(writeSize)
 }
 
-func (core *rotateCoreHandler) outputCore(file string, line int, lv level.Level, v ...interface{}) error {
-	// ロックファイルをつくったほうが良いが、OS 依存なので止めとく。
+func (core *rotateCoreHandler) flush() {
+	core.flushCore(0)
+}
 
-	buff := core.fmter.Format(time.Now(), file, line, lv, v...)
+var fileIsFull error = errors.New("file is full")
+
+func (core *rotateCoreHandler) flushCore(writeSize int) {
+	// ロックファイルをつくったほうが良いが、OS 依存なので止めとく。
 
 	for {
 		if core.file == nil {
 			// ファイルを開く。
 			if err := os.MkdirAll(filepath.Dir(core.path), dirPerm); err != nil {
-				return erro.Wrap(err)
+				fmt.Fprintln(os.Stderr, erro.Wrap(err))
+				return
 			}
 
 			var err error
 			core.file, err = os.OpenFile(core.path, os.O_RDWR|os.O_APPEND|os.O_CREATE, filePerm)
 			if err != nil {
-				return erro.Wrap(err)
+				fmt.Fprintln(os.Stderr, erro.Wrap(err))
+				return
 			}
 
-			stat, err := core.file.Stat()
-			if err != nil {
-				return erro.Wrap(err)
+			if err := core.buff.setSink(core.file); err != nil {
+				fmt.Fprintln(os.Stderr, erro.Wrap(err))
+				return
 			}
-			core.size = stat.Size()
 		}
 
 		// ファイルを開いている。
 
-		if core.size != 0 && core.size+int64(len(buff)) > core.limit { // ファイルは必ず 1 度は使う。
+		err := core.buff.flushIfNeeded(writeSize)
+		switch err {
+		case nil:
+			// 書き込み成功。
+			return
+		case fileIsFull:
 			// ローテートする。
-			if err := core.rotate(); err != nil {
-				return erro.Wrap(err)
+			core.file.Close()
+			core.file = nil
+			if err := rotateFile(core.path, core.num); err != nil {
+				fmt.Fprintln(os.Stderr, erro.Wrap(err))
+				return
 			}
 			continue
+		default:
+			fmt.Fprintln(os.Stderr, erro.Wrap(err))
+			core.file.Close()
+			core.file = nil
+			return
 		}
-
-		if core.sink == nil {
-			core.sink = bufio.NewWriter(core.file)
-		}
-
-		// ファイルに余裕がある。
-		break
 	}
-
-	size, err := core.sink.Write(buff)
-	if err != nil {
-		return erro.Wrap(err)
-	}
-
-	core.size += int64(size)
-	return nil
-}
-
-func (core *rotateCoreHandler) rotate() error {
-	if core.sink != nil {
-		core.sink.Flush()
-		core.sink = nil
-	}
-	if core.file != nil {
-		core.file.Close()
-		core.file = nil
-	}
-
-	return erro.Wrap(rotateFile(core.path, core.num))
 }
 
 func rotateFile(path string, num int) error {
@@ -152,26 +137,14 @@ func rotateFile(path string, num int) error {
 	return nil
 }
 
-func (core *rotateCoreHandler) flush() {
-	if core.sink == nil {
-		return
-	}
-	if err := core.sink.Flush(); err != nil {
-		err = erro.Wrap(err)
-		fmt.Fprintln(os.Stderr, err)
-	}
-}
-
 func (core *rotateCoreHandler) close() {
-	core.flush()
-
 	if core.file == nil {
 		return
 	}
-	if err := core.file.Close(); err != nil {
-		err = erro.Wrap(err)
-		fmt.Fprintln(os.Stderr, err)
-	}
+	core.flush()
+	core.buff.flush()
+	core.file.Close()
+	core.file = nil
 }
 
 func NewRotateHandler(path string, limit int64, num int) Handler {
@@ -179,5 +152,106 @@ func NewRotateHandler(path string, limit int64, num int) Handler {
 }
 
 func NewRotateHandlerUsing(path string, limit int64, num int, fmter Formatter) Handler {
-	return wrapCoreHandler(newSynchronizedCoreHandler(&rotateCoreHandler{path: path, limit: limit, num: num, fmter: fmter}))
+	return wrapCoreHandler(newSynchronizedCoreHandler(&rotateCoreHandler{
+		path:  path,
+		num:   num,
+		fmter: fmter,
+		buff:  newFileLogBuffer(int(limit)),
+	}))
+}
+
+// ファイル用のバッファ付き書き込みラッパー。
+type fileLogBuffer struct {
+	limit int // 32 ビット環境では 4GB 制限。
+
+	// 書き込みファイル。
+	sink     *os.File
+	sinkSize int
+
+	// まだ書き込んでない分。先頭が一番古いキュー。
+	unwritten     *list.List
+	unwrittenSize int
+}
+
+func newFileLogBuffer(limit int) *fileLogBuffer {
+	return &fileLogBuffer{
+		limit:     limit,
+		unwritten: list.New(),
+	}
+}
+
+func (buff *fileLogBuffer) setSink(sink *os.File) error {
+	stat, err := sink.Stat()
+	if err != nil {
+		return erro.Wrap(err)
+	}
+	buff.sink = sink
+	buff.sinkSize = int(stat.Size())
+	return nil
+}
+
+func (buff *fileLogBuffer) add(log []byte) {
+	buff.unwritten.PushBack(log)
+	buff.unwrittenSize += len(log)
+}
+
+// サイズが baseline 未満にならないところまで古いログを消す。
+func (buff *fileLogBuffer) trim(baseline int) {
+	for buff.unwrittenSize > baseline && buff.unwritten.Len() > 0 {
+		elem := buff.unwritten.Front()
+		log := elem.Value.([]byte)
+		if buff.unwrittenSize-len(log) < baseline {
+			return
+		}
+		buff.unwritten.Remove(elem)
+		buff.unwrittenSize -= len(log)
+		fmt.Fprintln(os.Stderr, "Drop log: "+string(log))
+	}
+}
+
+func (buff *fileLogBuffer) flushIfNeeded(unwrittenLimit int) error {
+	if buff.unwrittenSize <= unwrittenLimit {
+		return nil
+	}
+	return buff.flush()
+}
+
+func (buff *fileLogBuffer) flush() error {
+	n := 0
+	writeBuff := []byte{}
+	for elem := buff.unwritten.Front(); elem != nil; elem = elem.Next() {
+		log := elem.Value.([]byte)
+		if curSize := buff.sinkSize + len(writeBuff); curSize > 0 && // サイズオーバーしても 1 個は必ず書く。
+			curSize+len(log) > buff.limit {
+			break
+		}
+		writeBuff = append(writeBuff, log...)
+		n++
+	}
+
+	if _, err := buff.sink.Write(writeBuff); err != nil {
+		return erro.Wrap(err)
+	}
+
+	buff.sinkSize += len(writeBuff)
+	if n == buff.unwritten.Len() {
+		// 全部書き出した。
+		buff.unwritten.Init()
+		buff.unwrittenSize = 0
+		return nil
+	}
+
+	// n 個だけ書き出した。
+
+	for i := 0; i < n; i++ {
+		buff.unwritten.Remove(buff.unwritten.Front())
+	}
+	buff.unwrittenSize -= len(writeBuff)
+	return fileIsFull
+}
+
+func (buff *fileLogBuffer) close() {
+	buff.flush()
+	buff.trim(0)
+	*buff = fileLogBuffer{}
 }
